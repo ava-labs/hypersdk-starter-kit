c052dd984028417088a5aca25ba0e612c4581baf
containerman17 <8990432+containerman17@users.noreply.github.com>
2024-08-20 05:11:52 +0000

    use devnet deployment on EC2

--- web_wallet/src/const.ts
+++ web_wallet/src/const.ts
@@ -1,11 +1,15 @@
-export const DEVELOPMENT_MODE = typeof window === 'undefined' || window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
 export const HRP = 'morpheus'
 export const COIN_SYMBOL = "RED"
 export const DECIMAL_PLACES = 9
 export const MAX_TRANSFER_FEE = 10000000n
 
-export const FAUCET_HOST = DEVELOPMENT_MODE ? 'http://localhost:8765' : ''
-export const API_HOST = DEVELOPMENT_MODE ? 'http://localhost:9650' : ''
+let isDevMode = typeof window === 'undefined' || window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+isDevMode = false//FIXME:
+export const DEVELOPMENT_MODE = isDevMode
+
+const DEVNET_DEPLOYMENT_URL = "https://ec2-18-224-139-0.us-east-2.compute.amazonaws.com"
+export const FAUCET_HOST = DEVELOPMENT_MODE ? 'http://localhost:8765' : DEVNET_DEPLOYMENT_URL
+export const API_HOST = DEVELOPMENT_MODE ? 'http://localhost:9650' : DEVNET_DEPLOYMENT_URL
 
 
 import snapPkgJson from "sample-metamask-snap-for-hypersdk/package.json"
