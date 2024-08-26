b5208dad7aafa94f00861db1d8765807d4785010
containerman17 <8990432+containerman17@users.noreply.github.com>
2024-08-26 05:58:09 +0000

    return frontend back to vm

--- web_wallet/src/const.ts
+++ web_wallet/src/const.ts
@@ -6,9 +6,8 @@ export const MAX_TRANSFER_FEE = 10000000n
 let isDevMode = typeof window === 'undefined' || window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
 export const DEVELOPMENT_MODE = isDevMode
 
-const DEVNET_DEPLOYMENT_URL = "https://ec2-18-224-139-0.us-east-2.compute.amazonaws.com"
-export const FAUCET_HOST = DEVELOPMENT_MODE ? 'http://localhost:8765' : DEVNET_DEPLOYMENT_URL
-export const API_HOST = DEVELOPMENT_MODE ? 'http://localhost:9650' : DEVNET_DEPLOYMENT_URL
+export const FAUCET_HOST = DEVELOPMENT_MODE ? 'http://localhost:8765' : ""
+export const API_HOST = DEVELOPMENT_MODE ? 'http://localhost:9650' : ""
 
 
 import snapPkgJson from "sample-metamask-snap-for-hypersdk/package.json"
