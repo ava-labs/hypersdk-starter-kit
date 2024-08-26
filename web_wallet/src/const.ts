a9142c198381e8ccfccf29499699c6e0ca1f3149
Containerman17 <8990432+containerman17@users.noreply.github.com>
2024-08-26 16:55:44 +0900

    always use snap

--- web_wallet/src/const.ts
+++ web_wallet/src/const.ts
@@ -3,12 +3,11 @@ export const COIN_SYMBOL = "RED"
 export const DECIMAL_PLACES = 9
 export const MAX_TRANSFER_FEE = 10000000n
 
-let isDevMode = typeof window === 'undefined' || window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-export const DEVELOPMENT_MODE = isDevMode
+export const DEVELOPMENT_MODE = typeof window === 'undefined' || window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
 
 export const FAUCET_HOST = DEVELOPMENT_MODE ? 'http://localhost:8765' : ""
 export const API_HOST = DEVELOPMENT_MODE ? 'http://localhost:9650' : ""
 
 
 import snapPkgJson from "sample-metamask-snap-for-hypersdk/package.json"
-export const SNAP_ID = DEVELOPMENT_MODE ? 'local:http://localhost:8080' : `npm:${snapPkgJson.name}`
+export const SNAP_ID = `npm:${snapPkgJson.name}`//DEVELOPMENT_MODE ? 'local:http://localhost:8080' : `npm:${snapPkgJson.name}`
