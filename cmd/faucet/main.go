ed595066130c009764de117dabe4bca83e457f56
containerman17 <8990432+containerman17@users.noreply.github.com>
2024-08-29 10:23:24 +0000

    add morpheus, but no tests and no docker

--- cmd/faucet/main.go
+++ cmd/faucet/main.go
@@ -17,16 +17,16 @@ import (
 	"github.com/rs/cors"
 	"golang.org/x/time/rate"
 
+	"github.com/ava-labs/hypersdk-starter/morpheusvm/actions"
 	"github.com/ava-labs/hypersdk/api/jsonrpc"
 	"github.com/ava-labs/hypersdk/auth"
 	"github.com/ava-labs/hypersdk/chain"
 	"github.com/ava-labs/hypersdk/codec"
 	"github.com/ava-labs/hypersdk/crypto/ed25519"
-	"github.com/ava-labs/hypersdk/examples/morpheusvm/actions"
 	"github.com/ava-labs/hypersdk/utils"
 
-	lconsts "github.com/ava-labs/hypersdk/examples/morpheusvm/consts"
-	"github.com/ava-labs/hypersdk/examples/morpheusvm/controller"
+	lconsts "github.com/ava-labs/hypersdk-starter/morpheusvm/consts"
+	"github.com/ava-labs/hypersdk-starter/morpheusvm/controller"
 )
 
 const amtStr = "10.00"
---
+++ morpheusvm/registry/registry.go
@@ -0,0 +1,38 @@
+// Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package registry
+
+import (
+	"github.com/ava-labs/avalanchego/utils/wrappers"
+
+	"github.com/ava-labs/hypersdk-starter/morpheusvm/actions"
+	"github.com/ava-labs/hypersdk/auth"
+	"github.com/ava-labs/hypersdk/chain"
+	"github.com/ava-labs/hypersdk/codec"
+)
+
+var (
+	Action *codec.TypeParser[chain.Action]
+	Auth   *codec.TypeParser[chain.Auth]
+)
+
+// Setup types
+func init() {
+	Action = codec.NewTypeParser[chain.Action]()
+	Auth = codec.NewTypeParser[chain.Auth]()
+
+	errs := &wrappers.Errs{}
+	errs.Add(
+		// When registering new actions, ALWAYS make sure to append at the end.
+		Action.Register(&actions.Transfer{}, nil),
+
+		// When registering new auth, ALWAYS make sure to append at the end.
+		Auth.Register(&auth.ED25519{}, auth.UnmarshalED25519),
+		Auth.Register(&auth.SECP256R1{}, auth.UnmarshalSECP256R1),
+		Auth.Register(&auth.BLS{}, auth.UnmarshalBLS),
+	)
+	if errs.Errored() {
+		panic(errs.Err)
+	}
+}
