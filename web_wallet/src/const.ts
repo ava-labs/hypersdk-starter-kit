c118a34f1ce679b9e7f24dfc1d9de95f7e2aef6d
owenwahlgren <owenwahlgren@gmail.com>
2024-10-04 13:25:50 -0400

    update dev container

--- web_wallet/src/const.ts
+++ web_wallet/src/const.ts
@@ -2,7 +2,7 @@
 export const DEVELOPMENT_MODE = typeof window === 'undefined' || window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
 
 export const FAUCET_HOST = DEVELOPMENT_MODE ? 'http://localhost:8765' : ""
-export const API_HOST = DEVELOPMENT_MODE ? 'http://localhost:9651' : ""
+export const API_HOST = DEVELOPMENT_MODE ? 'http://localhost:9650' : ""
 
 export const VM_NAME = 'CFMMVM';
 export const VM_RPC_PREFIX = 'cfmmapi';
